4ad49d280b6f73e5a3099399a0e812e46bc7ce69
deztopia <jason.melendez@gmail.com>
2021-09-09 16:33:08 -0700

    update menu-items to nav withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 // components
@@ -7,7 +8,9 @@ import HomePage from './pages/homepage.component';
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
