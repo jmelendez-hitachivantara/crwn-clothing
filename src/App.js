f0c3d5c94be091f658b99f5aad61551281342757
deztopia <jason.melendez@gmail.com>
2021-09-14 18:37:54 -0700

    implemented email/password signup

--- src/App.js
+++ src/App.js
@@ -25,13 +25,12 @@ class App extends React.Component {
       if (userAuth) {
         const userRef= await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          console.log(snapShot.data());
           this.setState({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
-          });
+          }, () => console.log(this.state));
         });
       } else {
         this.setState({currentUser: null});
