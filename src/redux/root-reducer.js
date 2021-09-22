4b6631713d36cc3b97bb7d1121dd4000b29bf76f
deztopia <jason.melendez@gmail.com>
2021-09-21 18:00:49 -0700

    moved static data into reducers

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,8 @@ import storage from "redux-persist/lib/storage";
 // reducers
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 // redux persist for local storage persistance of cart
 const persistConfig = {
@@ -15,7 +17,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,5 +7,3 @@ import rootReducer from "./root-reducer";
 const middleware = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 export const persistor = persistStore(store);
-
-export default {store, persistor};
